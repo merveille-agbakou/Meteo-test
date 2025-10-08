d561d7394c61a70bf971eba3040963e061ebeaed
merveille-agbakou <merveilleagba@gmail.com>
2025-10-08 16:40:53 +0100

    separe date and ville hystories

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -19,15 +19,7 @@
                 <polyline points="9 22 9 12 15 12 15 22"></polyline>
               </svg>
               <span>Accueil</span>
-            </router-link>
-            <button v-if="currentWeather" @click="toggleFavorite(currentWeather)" class="favorite-btn" :class="{ 'is-favorite': isFavorite(currentWeather.id) }">
-              <svg v-if="isFavorite(currentWeather.id)" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-              </svg>
-              <svg v-else xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-              </svg>
-            </button>
+            </router-link>           
           </nav>
         </div>
       </div>
